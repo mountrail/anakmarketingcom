37c02d40ad143beeada6e3bfefe12fd624bddb57
mountrail <ahmadgunawan957@gmail.com>
2025-05-05 12:55:44 +0800

    create dropdown select input component

--- resources/views/profile/partials/update-profile-information-form.blade.php
+++ resources/views/profile/partials/update-profile-information-form.blade.php
@@ -68,54 +68,56 @@
             </p>
         </header>
 
+        {{-- Industry Field --}}
         <div>
             <x-input-label for="industry" :value="__('Industry')" />
-            <select id="industry" name="industry"
-                class="mt-1 block w-full border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-white rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500">
-                @foreach (['Beauty', 'Consumer', 'Education', 'Financial or Banking', 'Health', 'Media', 'Products', 'Property', 'Services', 'Tech', 'Others'] as $option)
-                    <option value="{{ $option }}"
-                        {{ old('industry', $user->industry) === $option ? 'selected' : '' }}>{{ $option }}
-                    </option>
-                @endforeach
-            </select>
+            <x-select-input id="industry" name="industry" :options="[
+                'Beauty',
+                'Consumer',
+                'Education',
+                'Financial or Banking',
+                'Health',
+                'Media',
+                'Products',
+                'Property',
+                'Services',
+                'Tech',
+                'Others',
+            ]" :selected="old('industry', $user->industry)"
+                class="mt-1 block w-full" />
             <x-input-error class="mt-2" :messages="$errors->get('industry')" />
         </div>
 
+        {{-- Seniority Field --}}
         <div>
             <x-input-label for="seniority" :value="__('Seniority')" />
-            <select id="seniority" name="seniority"
-                class="mt-1 block w-full border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-white rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500">
-                @foreach (['Junior Staff', 'Senior Staff', 'Assistant Manager', 'Manager', 'Vice President', 'Director (C-Level)', 'Owner', 'Others'] as $option)
-                    <option value="{{ $option }}"
-                        {{ old('seniority', $user->seniority) === $option ? 'selected' : '' }}>{{ $option }}
-                    </option>
-                @endforeach
-            </select>
+            <x-select-input id="seniority" name="seniority" :options="[
+                'Junior Staff',
+                'Senior Staff',
+                'Assistant Manager',
+                'Manager',
+                'Vice President',
+                'Director (C-Level)',
+                'Owner',
+                'Others',
+            ]" :selected="old('seniority', $user->seniority)"
+                class="mt-1 block w-full" />
             <x-input-error class="mt-2" :messages="$errors->get('seniority')" />
         </div>
 
+        {{-- Company Size Field --}}
         <div>
             <x-input-label for="company_size" :value="__('Company Size')" />
-            <select id="company_size" name="company_size"
-                class="mt-1 block w-full border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-white rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500">
-                @foreach (['0-10', '11-50', '51-100', '101-500', '501++'] as $option)
-                    <option value="{{ $option }}"
-                        {{ old('company_size', $user->company_size) === $option ? 'selected' : '' }}>
-                        {{ $option }}</option>
-                @endforeach
-            </select>
+            <x-select-input id="company_size" name="company_size" :options="['0-10', '11-50', '51-100', '101-500', '501++']" :selected="old('company_size', $user->company_size)"
+                class="mt-1 block w-full" />
             <x-input-error class="mt-2" :messages="$errors->get('company_size')" />
         </div>
 
+        {{-- City Field --}}
         <div>
             <x-input-label for="city" :value="__('City')" />
-            <select id="city" name="city"
-                class="mt-1 block w-full border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-white rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500">
-                @foreach (['Bandung', 'Jabodetabek', 'Jogjakarta', 'Makassar', 'Medan', 'Surabaya', 'Others'] as $option)
-                    <option value="{{ $option }}" {{ old('city', $user->city) === $option ? 'selected' : '' }}>
-                        {{ $option }}</option>
-                @endforeach
-            </select>
+            <x-select-input id="city" name="city" :options="['Bandung', 'Jabodetabek', 'Jogjakarta', 'Makassar', 'Medan', 'Surabaya', 'Others']" :selected="old('city', $user->city)"
+                class="mt-1 block w-full" />
             <x-input-error class="mt-2" :messages="$errors->get('city')" />
         </div>
 
