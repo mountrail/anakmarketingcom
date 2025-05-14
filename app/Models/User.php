6413ee2a7b34bd1c3bf1379ef6a010489eaa5101
mountrail <ahmadgunawan957@gmail.com>
2025-05-15 07:43:21 +0800

    spatie user model fix

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,9 +6,7 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Spatie\Permission\traits\HasRoles; {
-
-}
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable implements MustVerifyEmail
 {
